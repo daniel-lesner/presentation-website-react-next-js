11d7e4d31c10676ca749f1f7b35aca4d8e6ca342
Daniel Lesner <daniel.lesner@icloud.com>
2025-02-11 19:39:45 +0200

    Install prettier and correct all files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
   return (
@@ -19,16 +19,24 @@ export default function Header() {
 
       <div className="flex items-center space-x-5 font-bold">
         <nav className="hidden lg:flex space-x-6 text-gray-300 text-lg">
-          <Link href="#contact" className="hover:text-white">Contact</Link>
-          <Link href="#projects" className="hover:text-white">Projects</Link>
-          <Link href="#work" className="hover:text-white">Work</Link>
-          <Link href="#education" className="hover:text-white">Education</Link>
+          <Link href="#contact" className="hover:text-white">
+            Contact
+          </Link>
+          <Link href="#projects" className="hover:text-white">
+            Projects
+          </Link>
+          <Link href="#work" className="hover:text-white">
+            Work
+          </Link>
+          <Link href="#education" className="hover:text-white">
+            Education
+          </Link>
         </nav>
 
         <a
           href="/documents/Daniel-Lesner_Curriculum-Vitae.pdf"
           download="Daniel-Lesner_Curriculum-Vitae.pdf"
-          className="bold bg-primary-color text-black px-4 py-2 rounded-lg transition-all duration-300 hover:shadow-custom hover:bg-secondary-color"
+          className="bold bg-primary-color text-black px-4 py-2 rounded-lg transition-all duration-200 hover:shadow-custom hover:bg-secondary-color"
         >
           Download CV
         </a>
