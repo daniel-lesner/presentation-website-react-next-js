ae0ea59ebfff5e1bf01256d79abe67e11c248b17
Daniel Lesner <daniel.lesner@icloud.com>
2025-02-11 19:50:56 +0200

    Add contact section

---
+++ src/components/ContactSection.tsx
@@ -0,0 +1,54 @@
+import { DevicePhoneMobileIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
+import GithubIcon from './GithubIcon';
+import LinkedInIcon from './LinkedInIcon';
+
+export default function ContactSection() {
+  return (
+    <div className="flex flex-col items-center justify-center text-white p-8">
+      <h1 className="text-5xl font-bold mb-6">Daniel Lesner</h1>
+      <h1 id="contact" className="text-4xl text-zinc-400 mb-14">
+        Contact
+      </h1>
+
+      <div className="space-y-4 text-xl md:text-2xl lg:text-3xl gap-4 flex flex-col gap-4 items-center">
+        <div className="flex space-x-2 gap-6 items-center ">
+          <DevicePhoneMobileIcon className="size-10 md:size-12 lg:size-14 text-primary-color"></DevicePhoneMobileIcon>
+          <a className="hover:text-secondary-color" href="tel:0033676939082">
+            0033676939082
+          </a>
+        </div>
+        <div className="flex space-x-2 gap-6 items-center ">
+          <EnvelopeIcon className="size-10 md:size-12 lg:size-14 text-primary-color"></EnvelopeIcon>
+          <a
+            className="hover:text-secondary-color"
+            href="mailto:daniel.lesner@icloud.com"
+          >
+            daniel.lesner@icloud.com
+          </a>
+        </div>
+        <div className="flex space-x-2 gap-6 items-center ">
+          <LinkedInIcon />
+          <a
+            className="hover:text-secondary-color"
+            href="http://www.linkedin.com/in/daniel-lesner"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            linkedin.com/in/daniel-lesner
+          </a>
+        </div>
+        <div className="flex space-x-2 gap-6 items-center ">
+          <GithubIcon />
+          <a
+            className="hover:text-secondary-color"
+            href="http://www.github.com/daniel-lesner"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/daniel-lesner
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import Header from "@/components/Header";
+import ContactSection from '@/components/ContactSection';
+import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <div className="bg-background min-h-screen">
       <Header />
+      <ContactSection />
     </div>
   );
 }
