7aa820dc3d7910bcfc96c23ca5e8f53bfcd415db
Daniel Lesner <daniel.lesner@icloud.com>
2025-05-26 00:14:01 +0300

    Changed work section wording

--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -10,12 +10,12 @@ const experiences = [
     description: [
       "Developed and tested new features to enhance the company's SaaS solutions.",
       'Worked in a Test-Driven Development (TDD) environment.',
-      'Engaged in backporting and refactoring legacy code.',
       'Integrated third-party services like Sharepoint and Google Drive.',
+      'Engaged in backporting and refactoring legacy code.',
       'Participated in an Agile setup with bi-weekly sprints.',
     ],
     stack: {
-      Frontend: ['Ember.js', 'React.js', 'Bootstrap', 'Jest', 'XState'],
+      Frontend: ['Ember.js', 'React.js', 'Bootstrap', 'XState', 'Jest'],
       Backend: ['Ruby on Rails', 'Sidekiq', 'RSpec', 'JSONAPI-Resources'],
       Databases: ['MariaDB', 'Redis', 'ElasticSearch'],
       Others: ['Bash', 'Docker', 'Jenkins', 'Gitlab CI/CD'],
@@ -27,13 +27,12 @@ const experiences = [
     duration: '10/2021 - 04/2022 (6 months)',
     location: 'Bucharest, Romania',
     description: [
-      'Worked on a decentralized multi-blockchain application.',
-      'Learnt about blockchain, smart contracts & web3.js.',
-      'Worked with DeFi concepts like token creation and staking.',
+      'Developed a Next.js webapp for a multi-blockchain DeFi platform.',
+      'Integrated web3.js to interact with Ethereum smart contracts.',
+      'Researched DeFi concepts like token creation and staking.',
     ],
     stack: {
-      Frontend: ['TypeScript', 'React.js', 'Material-UI', 'Next.js'],
-      Backend: ['Node.js', 'Solidity'],
+      Frontend: ['TypeScript', 'React.js', 'Next.js', 'Material-UI'],
     },
   },
   {
@@ -42,13 +41,13 @@ const experiences = [
     duration: '11/2020 - 08/2021 (9 months)',
     location: 'Bucharest, Romania',
     description: [
-      "Supported partners in developing websites using VTEX's framework.",
+      'Supported partners in building e-commerce sites on the VTEX platform.',
       'Addressed technical and business requirements for clients.',
       'Managed SEO optimization, debugging, and project management.',
       'Gained knowledge about the digital commerce industry.',
     ],
     stack: {
-      Frontend: ['HTML', 'CSS', 'JavaScript', 'VTEX IO'],
+      Frontend: ['HTML', 'CSS', 'JavaScript'],
     },
   },
   {
@@ -57,10 +56,11 @@ const experiences = [
     duration: '10/2018 - 11/2020 (2 years)',
     location: 'Bucharest, Romania',
     description: [
-      'Worked on Test and Control studies for ad effectiveness.',
-      'Developed scripts to automate tasks.',
-      'Processed large databases and improved workflows.',
-      'Trained colleagues and wrote technical documentation.',
+      'Projects ranged from Test and Control studies (that measure advertisement effectiveness using geographic experiments) to more complex ones such as Marketing Mix Modelling studies (which help clients understand the impact of their advertising spend and forecast future marketing campaign performance).',
+      'Developed and automated tasks using custom scripts.',
+      'Followed and modified pre-made scripts.',
+      'Gathered, cleaned, and analyzed large datasets.',
+      'Wrote technical documentation and trained team members.',
     ],
     stack: {
       Programming: ['R', 'Python', 'SPSS', 'VBA', 'BASH', 'SQL'],
