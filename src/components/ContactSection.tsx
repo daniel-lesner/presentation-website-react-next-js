ca50edcb0962047add00abed5bdc5dfc39e4da8f
Daniel Lesner <daniel.lesner@icloud.com>
2025-05-30 11:29:21 +0300

    Change color when hovering contact text

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -13,14 +13,14 @@ export default function ContactSection() {
       <div className="space-y-4 text-xl md:text-2xl lg:text-3xl gap-4 flex flex-col gap-4 items-start">
         <div className="flex space-x-2 gap-6 items-center ">
           <DevicePhoneMobileIcon className="size-10 md:size-12 lg:size-14 text-primary-color fill-current animate-combined"></DevicePhoneMobileIcon>
-          <a className="hover:text-secondary-color" href="tel:0033676939082">
+          <a className="hover:text-primary-color" href="tel:0033676939082">
             0033676939082
           </a>
         </div>
         <div className="flex space-x-2 gap-6 items-center ">
           <EnvelopeIcon className="size-10 md:size-12 lg:size-14 text-primary-color fill-current animate-combined"></EnvelopeIcon>
           <a
-            className="hover:text-secondary-color"
+            className="hover:text-primary-color"
             href="mailto:daniel.lesner@icloud.com"
           >
             daniel.lesner@icloud.com
@@ -29,7 +29,7 @@ export default function ContactSection() {
         <div className="flex space-x-2 gap-6 items-center ">
           <LinkedInIcon className="text-primary-color fill-current animate-combined" />
           <a
-            className="hover:text-secondary-color"
+            className="hover:text-primary-color"
             href="http://www.linkedin.com/in/daniel-lesner"
             target="_blank"
             rel="noopener noreferrer"
@@ -40,7 +40,7 @@ export default function ContactSection() {
         <div className="flex space-x-2 gap-6 items-center ">
           <GithubIcon className="text-primary-color fill-current animate-combined" />
           <a
-            className="hover:text-secondary-color"
+            className="hover:text-primary-color"
             href="http://www.github.com/daniel-lesner"
             target="_blank"
             rel="noopener noreferrer"
