b31c271c9c96ba543f65f601ce780f043e6fbbd4
Daniel Lesner <daniel.lesner@icloud.com>
2025-05-30 16:14:45 +0300

    Fix Download CV button hovering looking weird after previous page redesign

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
         <a
           href="/documents/Daniel-Lesner_Curriculum-Vitae.pdf"
           download="Daniel-Lesner_Curriculum-Vitae.pdf"
-          className="bold bg-primary-color text-black px-4 py-2 rounded-lg transition-all duration-200 hover:shadow-custom hover:bg-secondary-color"
+          className="bold bg-primary-color text-black px-4 py-2 rounded-lg transition-all duration-200 hover:shadow-custom hover:bg-secondary-color hover:text-white hover:scale-110 hover:border-2 hover:border-purple-700 active:scale-90"
         >
           Download CV
         </a>
